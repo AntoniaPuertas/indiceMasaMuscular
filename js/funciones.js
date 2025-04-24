d5343678a7377b9e1f983928e0a1032afa573800
Totilla <tsm.antonia@gmail.com>
2025-04-24 13:18:32 +0200

    validación de datos

--- js/funciones.js
+++ js/funciones.js
@@ -23,7 +23,13 @@ function calcularIMC(){
     let peso = document.getElementById("peso").value;
     let altura = document.getElementById("altura").value;
     
-    //TODO: validar los datos de entrada
+   
+    let datosValidados = validarDatos(nombre, peso, altura);
+    //si los datos no  son correctos, mostrar mensaje
+    if(!datosValidados.sonValidos){
+        document.getElementById("mostrarErrores").innerHTML = datosValidados.mensaje;
+        return;
+    }
 
     //convierto la altura a metros
     altura = altura / 100;
@@ -66,3 +72,31 @@ function calcularIMC(){
         document.getElementById("resultadoEstudio").innerHTML = '';
         document.getElementById("resultadoEstudio").style.backgroundColor = 'transparent';
     })
+
+    //validar los datos de entrada
+    function validarDatos(nombre, peso, altura){
+
+        let resultado = {
+            mensaje: '',
+            sonValidos: true
+        }
+        //nombre debe contener algo
+        //peso debe ser mayor que 0 y menor que 350
+        //altura debe ser mayor que 0 y menor que 210
+        if(nombre.trim() === ''){
+            resultado.mensaje = "<p>Debe introducir un nombre</p>";
+            resultado.sonValidos = false;
+        }
+
+        if(peso <= 0 || peso > 350){
+            resultado.mensaje += "<p>El peso debe estar comprendido entre 0 y 350 kg</p>";
+            resultado.sonValidos = false;
+        }
+
+        if(altura <= 0 || altura > 210){
+            resultado.mensaje += "<p>La altura debe estar comprendida entre 0 y 210 cm</p>";
+            resultado.sonValidos = false;
+        }
+
+        return resultado;
+    }
