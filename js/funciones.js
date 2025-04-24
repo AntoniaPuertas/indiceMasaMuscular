82f1cbf1e59c540b8d4b06b570b7facc1725895d
Totilla <tsm.antonia@gmail.com>
2025-04-24 10:47:00 +0200

    iconos  y estilos añadidos

--- js/funciones.js
+++ js/funciones.js
@@ -9,6 +9,11 @@ let verde = 'rgb(82, 190, 65)';
 let rojo = 'rgb(228, 75, 75)';
 let naranja = 'rgb(231, 130, 72)';
 
+//variables para los iconos
+let iconoVerde = '🫠';
+let iconoRojo = '😒';
+let iconoNaranja = '😐';
+
 //evento para el button
 document.getElementById("calcular").addEventListener('click', calcularIMC);
 
@@ -30,15 +35,34 @@ function calcularIMC(){
     imc = imc.toFixed(2);
 
     //creo el mensaje de salida
-    let mensajeTxt = `El paciente ${nombre} tiene un IMC de ${imc}. `;
+    let mensajeTxt = `El paciente ${nombre} tiene un IMC de ${imc} `;
 
     //añado el resultado del análisis al mensaje
     if(imc < 18.5){
-        mensajeTxt += "<p>Esto indica un peso por debajo del ideal</p>";
+        mensajeTxt += `<span>${iconoRojo}</span> <p>Esto indica un peso por debajo del ideal</p>`;
+        document.getElementById("resultadoEstudio").style.backgroundColor = rojo;
     }else if(imc >= 18.5 && imc < 25){
-        mensajeTxt += "<p>Enhorabuena!! estás en tu peso ideal</p>"
+        mensajeTxt += `<span>${iconoVerde}</span> <p>Enhorabuena!! estás en tu peso ideal</p>`;
+        document.getElementById("resultadoEstudio").style.backgroundColor = verde;
     }else{
-        mensajeTxt += "<p>Tienes sobrepeso</p>";
+        mensajeTxt += `<span>${iconoNaranja}</span> <p>Tienes sobrepeso</p>`;
+        document.getElementById("resultadoEstudio").style.backgroundColor = naranja;
     }
     document.getElementById("resultadoEstudio").innerHTML = mensajeTxt;
 }
+
+    // limpiar la salida cuando cambien los datos del formulario
+    document.getElementById("nombre").addEventListener('input', function(){
+        document.getElementById("resultadoEstudio").innerHTML = '';
+        document.getElementById("resultadoEstudio").style.backgroundColor = 'transparent';
+    })
+    
+    document.getElementById("peso").addEventListener('input', function(){
+        document.getElementById("resultadoEstudio").innerHTML = '';
+        document.getElementById("resultadoEstudio").style.backgroundColor = 'transparent';
+    })
+
+    document.getElementById("altura").addEventListener('input', function(){
+        document.getElementById("resultadoEstudio").innerHTML = '';
+        document.getElementById("resultadoEstudio").style.backgroundColor = 'transparent';
+    })
